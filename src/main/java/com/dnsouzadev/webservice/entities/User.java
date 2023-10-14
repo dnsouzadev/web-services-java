c42305a008d254c4ef2921692d1031583c186b49
Daniel Souza <workdndsza@gmail.com>
2023-10-13 21:56:08 -0300

    criando e configurando o h2

--- src/main/java/com/dnsouzadev/webservice/entities/User.java
+++ src/main/java/com/dnsouzadev/webservice/entities/User.java
@@ -4,9 +4,15 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
