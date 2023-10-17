a890a4af35a91a81e611e1c6b8950d9fdb190fc3
Daniel Souza <workdndsza@gmail.com>
2023-10-16 23:57:21 -0300

    criando o findById

--- src/main/java/com/dnsouzadev/webservice/resources/UserResource.java
+++ src/main/java/com/dnsouzadev/webservice/resources/UserResource.java
@@ -5,10 +5,12 @@ import com.dnsouzadev.webservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,5 +22,11 @@ public class UserResource {
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
+        Optional<User> obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/dnsouzadev/webservice/services/UserService.java
+++ src/main/java/com/dnsouzadev/webservice/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +15,9 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public Optional<User> findById(long id) {
+        return repository.findById(id);
     }
 }
